1f08da93dace27fbc96dfe799eb93a4f6b2ed42c
wisamselim <wisamseelim79@gmail.com>
2022-10-02 21:36:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is my express app");
+  res.send("This is my first running app");
 });
 
 app.get("/me", (req, res) => {
-  res.send("Hi I am Laith");
+  res.send("Hi I am wisam saleem abdullah ");
 });
 
 app.listen(5000, () => {
